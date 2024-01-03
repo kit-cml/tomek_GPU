536597e34017d5459f9585e206c2ba0cfbecca10
Iga-CML_Linux <iga.narendra@gmail.com>
2024-01-03 15:06:25 +0900

    delete comments

--- bin/result/convert_init_format.py
+++ bin/result/convert_init_format.py
@@ -16,12 +16,11 @@ sample_size = 2000
 print("Parsing: "+filename)
 
 data = pd.read_csv(filename,header = None)
-# pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
 foldername = 'converted_'+filename.split('/')[1].split('c')[0]
 
 os.mkdir(foldername)
-# cnt = 0
+
 for indeks in range(core):
      f = open(foldername+"/output."+str(indeks), "w")
      f.write(str(data[42][0]*2000.00)+"\n")
@@ -33,6 +32,6 @@ with open(filename, 'r') as csvfile:
         indeks = int(row[0])/(sample_size/core)
         f = open(foldername+"/output."+str(int(indeks)), "a")
         for index in range(1,41):
-            f.write(str(row[index])+"0,")
-        f.write(f"{row[index]}0\n")
+            f.write(str(row[index])+",") 
+        f.write(f"{row[index]}\n")
         f.close()
