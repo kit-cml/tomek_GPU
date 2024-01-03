4a95a01328654d76f1a95a7fef9acce63d9a4ee0
Iga-CML_Linux <iga.narendra@gmail.com>
2024-01-03 15:06:12 +0900

    fix way of simulating cpu multicore

--- bin/result/convert_init_format.py
+++ bin/result/convert_init_format.py
@@ -1,33 +1,38 @@
 import os
 import argparse
+import csv
 import pandas as pd
 
 parser = argparse.ArgumentParser()
 
-#-db DATABASE -u USERNAME -p PASSWORD -size 20000
 parser.add_argument("-init_file", dest = "filename", help="initial file you want to convert")
 
 args = parser.parse_args()
 filename = args.filename
 
+core = 10
+sample_size = 2000
+
 print("Parsing: "+filename)
 
 data = pd.read_csv(filename,header = None)
+# pd.set_option('display.float_format', lambda x: '%.5f' % x)
 
 foldername = 'converted_'+filename.split('/')[1].split('c')[0]
 
 os.mkdir(foldername)
-
-for indeks in range(10):
-     f = open(foldername+"/output."+str(indeks), "a")
-     f.write(str(data[0][42]*2000.00)+"\n")
+# cnt = 0
+for indeks in range(core):
+     f = open(foldername+"/output."+str(indeks), "w")
+     f.write(str(data[42][0]*2000.00)+"\n")
      f.close()
-
-for idx,row in data.iterrows():
-    # print(row[1])
-    # devided as core (10 cores..)
-    f = open(foldername+"/output."+str(int(idx/200)), "a")
-    for index in range(1,41):
-        f.write(str(row[index])+",")
-    f.write(str(row[index+1])+"\n")
-    f.close()
+        
+with open(filename, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        indeks = int(row[0])/(sample_size/core)
+        f = open(foldername+"/output."+str(int(indeks)), "a")
+        for index in range(1,41):
+            f.write(str(row[index])+"0,")
+        f.write(f"{row[index]}0\n")
+        f.close()
