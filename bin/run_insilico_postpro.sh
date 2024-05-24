671cd7b74f0ec314e596c1a942ef93cd79b14dc4
IritaSee <iga.narendra@gmail.com>
2024-05-24 16:30:22 +0900

    add shellscript to run both mode, rename input_deck

---
+++ bin/run_insilico_postpro.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Read the file and extract the value of 'location'
+drugname=$(grep 'Drug_Name ' ./input_deck.txt | awk -F'= ' '{print $2}')
+concentration=$(grep 'Concentrations ' ./input_deck.txt | awk -F'= ' '{print $2}')
+formatted_concentration=$(printf "%.2f" "$concentration")
+
+echo "run in-silico"
+
+"./drug_sim" -input_deck ./input_deck.txt -hill_file ./drugs/$drugname/IC50_samples.csv
+
+echo "---------------------"
+echo "run post-processing"
+
+new_value="1"
+sed -i.old "s/^\(Is_Post_Processing = \).*/\1$new_value/" "./input_deck.txt"
+
+"./drug_sim" -input_deck ./input_deck.txt -hill_file ./drugs/$drugname/IC50_samples.csv -init_file ./result/$formatted_concentration.csv
+new_value="0"
+sed -i.old "s/^\(Is_Post_Processing = \).*/\1$new_value/" "./input_deck.txt"
